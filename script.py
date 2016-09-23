cb6a39ae50d22e109770b65eed63a06d4aeb551f
DANIYAR555 <estimator555@gmail.com>
2016-09-23 20:36:46 +0600

    Add script.py

---
+++ script.py
@@ -0,0 +1,321 @@
+
+print "Hello World"
+ p="Hello World"
+print p
+ a+12
+13
+ "name"+p
+'nameHello World'
+ help
+Type help() for interactive help, or help(object) for help about object.
+ help()
+
+
+x=1
+x=1.5
+ print x
+1.5
+ p='ddd'
+ p="ddddd"
+ p="""ddddd"""
+ p="""gdgdfgdfgdfgg
+... dfgdfgdfg
+... fgdgdfg
+... dfgdfgdfg
+... gdfgdfgdf
+... print p
+... p
+... print p
+
+>>> t = (1,5,7,(1,2,3,4))
+>>> c = [1,2,3,4,5,6]
+>>> len(d)
+3
+>>> len(t)
+4
+>>> len(c)
+6
+>>> 6 in c
+True
+>>> 7 in c
+False
+>>> min(c)
+1
+>>> max(c)
+6
+>>> del c[0]
+>>> c[0]
+2
+>>> c.append(2)
+>>> c
+[2, 3, 4, 5, 6, 2]
+>>> c.pop(2)
+4
+>>> c
+[2, 3, 5, 6, 2]
+>>> 'str'.reverse()
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: 'str' object has no attribute 'reverse'
+>>> if 1>0:
+...     print "Yes"
+... else:
+...     print 'no'
+... elif 2>0:
+  File "<stdin>", line 5
+    elif 2>0:
+       ^
+SyntaxError: invalid syntax
+>>> elif 2>0 
+  File "<stdin>", line 1
+    elif 2>0 
+       ^
+SyntaxError: invalid syntax
+>>> x=0
+>>> x
+0
+>>> x==0
+True
+>>> x==1
+False
+>>> x = int(raw_inpit('input key'))
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'raw_inpit' is not defined
+>>> for x in a:
+... print x, len(x)
+  File "<stdin>", line 2
+    print x, len(x)
+        ^
+IndentationError: expected an indented block
+>>> a
+1
+>>> p = 'sdsdsdsds'
+>>> for q in p.split():
+...     print len(q)
+... 
+9
+>>> 
+>>> q=[1,2,3,4]
+>>> for e in q:
+... print e+=1
+  File "<stdin>", line 2
+    print e+=1
+        ^
+IndentationError: expected an indented block
+>>> for e in q:
+...     print e+1
+... 
+2
+3
+4
+5
+>>> for i in 'string'
+  File "<stdin>", line 1
+    for i in 'string'
+                    ^
+SyntaxError: invalid syntax
+>>> for i in 'string'
+  File "<stdin>", line 1
+    for i in 'string'
+                    ^
+SyntaxError: invalid syntax
+>>> for i in 'string':
+... print i
+  File "<stdin>", line 2
+    print i
+        ^
+IndentationError: expected an indented block
+>>> for i in 'string':
+...     if i=='r':
+...        break
+...     print i
+... 
+s
+t
+>>> for i in 'string':
+...     in i=='r':
+  File "<stdin>", line 2
+    in i=='r':
+     ^
+SyntaxError: invalid syntax
+>>> for i in 'string':
+...     print i
+...     print i*5
+... 
+s
+sssss
+t
+ttttt
+r
+rrrrr
+i
+iiiii
+n
+nnnnn
+g
+ggggg
+>>> for 1 in 'string':
+...     print 1
+...     print 1*5
+...     if 1=='t':
+...        continue
+...     elif 1=='o':
+...        breake
+...     else:      print i
+>>> t = (1,5,7,(1,2,3,4))
+>>> c = [1,2,3,4,5,6]
+>>> len(d)
+3
+>>> len(t)
+4
+>>> len(c)
+6
+>>> 6 in c
+True
+>>> 7 in c
+False
+>>> min(c)
+1
+>>> max(c)
+6
+>>> del c[0]
+>>> c[0]
+2
+>>> c.append(2)
+>>> c
+[2, 3, 4, 5, 6, 2]
+>>> c.pop(2)
+4
+>>> c
+[2, 3, 5, 6, 2]
+>>> 'str'.reverse()
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: 'str' object has no attribute 'reverse'
+>>> if 1>0:
+...     print "Yes"
+... else:
+...     print 'no'
+... elif 2>0:
+  File "<stdin>", line 5
+    elif 2>0:
+       ^
+SyntaxError: invalid syntax
+>>> elif 2>0 
+  File "<stdin>", line 1
+    elif 2>0 
+       ^
+SyntaxError: invalid syntax
+>>> x=0
+>>> x
+0
+>>> x==0
+True
+>>> x==1
+False
+>>> x = int(raw_inpit('input key'))
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'raw_inpit' is not defined
+>>> for x in a:
+... print x, len(x)
+  File "<stdin>", line 2
+    print x, len(x)
+        ^
+IndentationError: expected an indented block
+>>> a
+1
+>>> p = 'sdsdsdsds'
+>>> for q in p.split():
+...     print len(q)
+... 
+9
+>>> 
+>>> q=[1,2,3,4]
+>>> for e in q:
+... print e+=1
+  File "<stdin>", line 2
+    print e+=1
+        ^
+IndentationError: expected an indented block
+>>> for e in q:
+...     print e+1
+... 
+2
+3
+4
+5
+>>> for i in 'string'
+  File "<stdin>", line 1
+    for i in 'string'
+                    ^
+SyntaxError: invalid syntax
+>>> for i in 'string'
+  File "<stdin>", line 1
+    for i in 'string'
+                    ^
+SyntaxError: invalid syntax
+>>> for i in 'string':
+... print i
+  File "<stdin>", line 2
+    print i
+        ^
+IndentationError: expected an indented block
+>>> for i in 'string':
+...     if i=='r':
+...        break
+...     print i
+... 
+s
+t
+>>> for i in 'string':
+...     in i=='r':
+  File "<stdin>", line 2
+    in i=='r':
+     ^
+SyntaxError: invalid syntax
+>>> for i in 'string':
+...     print i
+...     print i*5
+... 
+s
+sssss
+t
+ttttt
+r
+rrrrr
+i
+iiiii
+n
+nnnnn
+g
+ggggg
+>>> for 1 in 'string':
+...     print 1
+...     print 1*5
+...     if 1=='t':
+...        continue
+...     elif 1=='o':
+...        breake
+...     else:      print i
+
+... 
+... 
+... python
+... """
+
+
+
+
+print'a' in 'hello'
+print'a' in 'hello'
+print h.replace('l', 'p')
+print h.split('e')
+ h="hello"
+h.replace("l","p")
+print h[1]
+
+
+
+
